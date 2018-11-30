350304f3abf4106001a1bb6a7277e2cced6346bf
Vicky <vicky.chandnani@enbraun.com>
2018-11-30 19:40:51 +0530

    publish: logo changes
    generated from commit aac6c3975e97024f0050a2924217e3d973353af8

---
+++ javascripts/ers.js
@@ -0,0 +1,1 @@
+function myFunction(){document.getElementById("demo").innerHTML="Thank you for helping improve eRS Cloud's documentation. If you need help or have any questions, please consider contacting support."}
